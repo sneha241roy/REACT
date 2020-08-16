03756d3959031afb89585d18c14006b09979563b
Vikas Kotari <vkotari@gainsight.com>
2020-08-17 00:13:13 +0530

    React Project

---
+++ SNEHAREACTPROJECT/redux--01-start/src/containers/Counter/Counter.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import CounterControl from '../../components/CounterControl/CounterControl';
+import CounterOutput from '../../components/CounterOutput/CounterOutput';
+
+class Counter extends Component {
+    // state = {
+    //     counter: 0
+    // }
+
+    // counterChangedHandler = ( action, value ) => {
+    //     switch ( action ) {
+    //         case 'inc':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
+    //             break;
+    //         case 'dec':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
+    //             break;
+    //         case 'add':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
+    //             break;
+    //         case 'sub':
+    //             this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
+    //             break;
+    //     }
+    // }
+
+    render() {
+        return (
+            <div>
+                <CounterOutput value={this.props.ctr} />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter} />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter} />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter} />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                {this.props.storedResults.map((result) => {
+                        return <li key = {result.id} onClick={()=>this.props.onDeleteResult(result.id)}>{result.value}</li>
+                })}
+                </ul>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        ctr: state.counter,
+        storedResults: state.results
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onIncrementCounter: () => dispatch({ type: 'INCREMENT' }),
+        onDecrementCounter: () => dispatch({ type: 'DECREMENT' }),
+        onAddCounter: () => dispatch({ type: 'ADD', value: 5 }),
+        onSubtractCounter: () => dispatch({ type: 'SUBTRACT', value: 5 }),
+        onStoreResult: () => dispatch({ type: 'STORE_RESULT' }),
+        onDeleteResult: (id) => dispatch({ type: 'DELETE_RESULT', resultEleId:id })
+
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
---
+++ SNEHAREACTPROJECT/redux--01-start/src/store/reducer.js
@@ -0,0 +1,50 @@
+const initialState = {
+    counter: 0,
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+
+        case 'DECREMENT':
+            return {
+                ...state,
+                counter: state.counter - 1
+            }
+
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.value
+            }
+
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.value
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({ id: new Date(), value: state.counter })
+            }
+
+        case 'DELETE_RESULT':
+            const updatedResultsArray = state.results.filter(result => result.id !== action.resultEleId)
+            return {
+                ...state,
+                results: updatedResultsArray
+            }
+
+        default: return state
+    }
+
+
+}
+
+export default reducer
